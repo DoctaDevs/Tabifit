0046151ca3a00d5e9f245366f108d9be72a3383c
cristopherperez <cristopherperez88@gmail.com>
2020-05-22 19:45:58 -0300

    cambie nombres y elimine elementos

--- utillidades.js
+++
@@ -1,6 +0,0 @@
-function cargarTemplate(referenciaTemplate, instanciaComponente) {
-    const template = document.querySelector(referenciaTemplate);
-    const templateContent = template.content;
-    instanciaComponente.attachShadow({ mode: "open" })
-      .appendChild(templateContent.cloneNode(true));
-  }
